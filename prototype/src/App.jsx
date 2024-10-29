4c92f07c34959febc99523b61bf9bf3bd22a1ff4
Jack Young (1000112537) <YOUNJO1@student.op.ac.nz>
2024-10-30 11:07:23 +1300

    feat: select of all weeks in semester

--- prototype/src/App.jsx
+++ prototype/src/App.jsx
@@ -8,7 +8,22 @@ function App() {
     <>
       <input type="search"></input>
       <select>
-        <option value="1">Week 1</option>
+        <option value="1" disabled="true">Week 1</option>
+        <option value="2" disabled="true">Week 2</option>
+        <option value="3" disabled="true">Week 3</option>
+        <option value="4" disabled="true">Week 4</option>
+        <option value="5" disabled="true">Week 5</option>
+        <option value="6" disabled="true">Week 6</option>
+        <option value="7" disabled="true">Week 7</option>
+        <option value="8" disabled="true">Week 8</option>
+        <option value="9" disabled="true">Week 9</option>
+        <option value="10" disabled="true">Week 10</option>
+        <option value="11" disabled="true">Week 11</option>
+        <option value="12" disabled="true">Week 12</option>
+        <option value="13" disabled="true">Week 13</option>
+        <option value="14" selected="selected">Week 14</option>
+        <option value="15">Week 15</option>
+        <option value="16">Week 16</option>
       </select>
     </>
   )
