cce886d7c51cfa8494223563b7f6e273ceaf5f59
Jack Young (1000112537) <YOUNJO1@student.op.ac.nz>
2024-10-30 11:02:04 +1300

    init react application

---
+++ prototype/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <input type="search"></input>
+      <select>
+        <option value="1">Week 1</option>
+      </select>
+    </>
+  )
+}
+
+export default App
